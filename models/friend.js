673ef2efa5a3d4b662f1ca956ca88a015793b128
zym242 <zymfrank@hotmail.com>
2014-11-17 11:54:16 +1030

    final

--- models/friend.js
+++ models/friend.js
@@ -1,5 +1,5 @@
 var friends=[
-{"id":1,"name":"包林霏","day":17,"month":11,"year":2014,"remindOnBirthday":true,"remindBeforeBirthday":true},{"id":2,"name":"菜","day":17,"month":1,"year":2014,"remindOnBirthday":true,"remindBeforeBirthday":true},{"id":3,"name":"蔡瑜","day":17,"month":11,"year":2014,"remindOnBirthday":true,"remindBeforeBirthday":true}]
+{"id":1,"name":"包林霏","day":17,"month":11,"year":2014,"remindOnBirthday":true,"remindBeforeBirthday":true},{"id":2,"name":"菜","day":17,"month":1,"year":2014,"remindOnBirthday":true,"remindBeforeBirthday":true},{"id":3,"name":"蔡瑜","day":17,"month":11,"year":2014,"remindOnBirthday":true,"remindBeforeBirthday":true}
 ]
 
 module.exports = friends
