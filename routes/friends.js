4a2dd18a0dd64e30e64a9798ad86a21462fcea1b
zym242 <zymfrank@hotmail.com>
2014-11-17 15:21:14 +1030

    last chance

--- routes/friends.js
+++ routes/friends.js
@@ -6,9 +6,8 @@ app.post('/api/syncFriends',function(req,res){
   var friends = req.body.friends;
   var remindOnBirthday=req.body.remindOnBirthday;
   var remindBeforeBirthday=req.body.remindBeforeBirthday
-  Friend=[]
   for (var i=0;i<friends.length;i++){
-  	var elem={id:friends[i].id,name:friends[i].name,day:friends[i].day,month:friends[i].month,year:friends[i].year,remindOnBirthday:remindOnBirthday,remindBeforeBirthday:remindBeforeBirthday};
+  	var elem={"id":friends[i].id,"name":friends[i].name,"day":friends[i].day,"month":friends[i].month,"year":friends[i].year,"remindOnBirthday":remindOnBirthday,"remindBeforeBirthday":remindBeforeBirthday};
 	Friend.push(elem)
   }
   res.end();
